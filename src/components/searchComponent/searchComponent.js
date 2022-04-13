5a7fea43ccbd5e7dfc9a7bee808615204ce31883
Linear-DX <mariolinear97@gmail.com>
2022-04-13 09:39:30 +0700

    adding material UI

--- src/components/searchComponent/searchComponent.js
+++ src/components/searchComponent/searchComponent.js
@@ -1,4 +1,7 @@
 import '../../App.css';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import { cyan } from '@mui/material/colors';
 
 // const SearchComponent = ({ handleSubmit, handleInput, keyword }) => {
 //     return (
@@ -10,11 +13,25 @@ import '../../App.css';
 // }
 
 //edit
+
+const ColorButton = styled(Button)(({ theme }) => ({
+    color: theme.palette.getContrastText(cyan[500]),
+    backgroundColor: cyan[500],
+    borderColor: '#ccc',
+    '&:hover': {
+      backgroundColor: cyan[700],
+      borderColor: '#030302',
+      color: '#030302',
+    },
+  }));
+
 const SearchComponent = ({ handleSubmit, query, handleInput }) => {
     return (
-        <form onSubmit={handleSubmit} className="search-wrapper">
+        <form onSubmit={handleSubmit}>
+            {/* <div className="search-wrapper"> */}
             <input id="search" type="text" value={query} onChange={handleInput} required/>
-            <button>Search</button>
+            <ColorButton variant="outlined" size="small" onClick={handleSubmit}>Search</ColorButton>
+            {/* </div> */}
         </form>
     )
 }
